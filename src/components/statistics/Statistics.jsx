c1918436738b1bfba15afae4cf29d0a4f1e38132
Kaka8548 <katyakatya8548@gmail.com>
2022-12-14 00:15:18 +0400

    Task 2: done

---
+++ src/components/statistics/Statistics.jsx
@@ -0,0 +1,29 @@
+import css from './Statistics.module.css';
+import { createRandomColor } from '../../utils';
+import PropTypes from 'prop-types';
+
+export default function Statistics({ title = null, stats }) {
+  return (
+    <div className={css.statisticsWrapper}>
+      <section className={css.statistics}>
+        {title && <h2 className={css.title}>{title}</h2>}
+
+        <ul className={css.statList}>
+          {stats.map(stat => (
+            <li
+              key={stat.id}
+              className={css.item}
+              style={{
+                backgroundColor: createRandomColor(),
+              }}
+            >
+              <span className={css.label}>{stat.label}</span>
+              <span className={css.percentage}>{stat.percentage}</span>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+}
+
